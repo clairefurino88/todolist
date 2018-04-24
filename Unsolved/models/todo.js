0bfc202554e7e549b059c645919c11091ca1b693
clairefurino88 <33135335+clairefurino88@users.noreply.github.com>
2018-04-24 14:34:23 -0400

    added functionality

---
+++ Unsolved/models/todo.js
@@ -0,0 +1,20 @@
+module.exports = function (sequelize, DataTypes) {
+  var Todo = sequelize.define("Todo", {
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    complete: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+      timestamps: false
+    }
+  );
+
+  return Todo;
+};
